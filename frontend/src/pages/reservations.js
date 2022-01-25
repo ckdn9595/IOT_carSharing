64f5c86a0826e5b9d8f18cf5146d19bb265e4464
이지우 <ckdn9595@ssafy.com>
2022-01-25 14:09:09 +0900

    Design: ..recovery pages/..

---
+++ frontend/src/pages/reservations.js
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+import { Box, Container } from '@mui/material';
+import { CustomerListResults } from '../components/customer/customer-list-results';
+import { CustomerListToolbar } from '../components/customer/customer-list-toolbar';
+import { DashboardLayout } from '../components/dashboard-layout';
+import { customers } from '../__mocks__/customers';
+import Insurance from 'src/components/car-management/insurance/insurance';
+import CarReview from 'src/components/car-management/review/car-review-create';
+import Review from 'src/components/car-management/review/review'
+import ReviewList from 'src/components/car-management/review/review-list'
+import CarRegister from 'src/components/car-management/car-register';
+
+const Customers = () => (
+  <>
+    <Head>
+      <title>
+        Customers | Material Kit
+      </title>
+    </Head>
+    {/* <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <CustomerListToolbar />
+        <Box sx={{ mt: 3 }}>
+          <CustomerListResults customers={customers} />
+        </Box>
+      </Container>
+    </Box> */}
+    <Insurance/>
+    <CarReview/>
+    <ReviewList/>
+    <Review/>
+    <CarRegister/>
+
+  </>
+);
+Customers.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Customers;
