7a80c9e68e4fcdab1ec58188be1540913fe30b0b
전현민 <ssiluk12345@gmail.com>
2022-02-07 20:16:08 +0900

    Fix: Fix git branch issue
    Remake feature/backend-car branch from develop branch
    
    Resolves: S06P12A104-54

--- backend/routes/car.js
+++ backend/routes/car.js
@@ -1,40 +1,70 @@
 const express = require('express');
 
-const fs = require('fs');
-const path = require('path');
+const db = require('../models/index');
 
-const multer = require('multer');
-// const {upload} = require('../utils/multer');
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, 'uploads/');
-        },
-        filename: (req, file, cb) => {
-            const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + '_' + Date.now() + ext);
-        }
-    }),
-    // limits: {fileSize: 10 * 1024 * 1024}
-})
+const {upload} = require('../utils/multer');
+
+const jwt = require('jsonwebtoken');
+process.env.JWT_SECRET = "secret";
 
 // api/car
 const router = express.Router();
 
 
 // 차량등록 API
-router.post('/register', upload.single('carRegisterImage'), async(req, res) => {
+router.post('/register', upload.single('carImg'), async(req, res) => {
     console.log(req.body);
     try {
-        if (req.file.mimetype === 'image/png' || req.file.mimetype === 'image/jpg') {
-            return res.json({carRegister:'success'})
+        // JWT 유효성 검증
+        const decodedUserToken = await jwt.verify(req.body['userToken'], process.env.JWT_SECRET);
+
+        if (decodedUserToken) {
+            if (decodedUserToken.exp < Date.now()/1000) {
+                return res.status(400).json({message: 'jwt expired'});
+            }
         }
         else {
-            return res.json({carRegister:'fail: Upload jpg/png file ONLY!'})
+            return res.status(400).json({message: 'invalid jwt'});
+        }
+
+        // 받아온 carImg가 .png || .jpeg이 아닌 경우 에러
+        if (req.file.mimetype !== 'image/png' && req.file.mimetype !== 'image/jpeg') {
+            return res.status(400).json({status_code: 1});
+        }
+
+        // 필요한 항목이 모두 req.body에 있는 경우 진행
+        if (req.body['userToken', 'carNum', 'carModel', 'carYear', 'carSegment', 'carFuel', 'carRate', 'carImg', 'rentInsurance']) {
+            // 동일한 차량번호가 이미 DB에 존재하는 지 확인
+            if (await db['tb_car'].findOne({where: {car_num: req.body['carNum']}}) != null) {
+                return res.status(400).json({message: 'carNum is already in use'});
+            }
+
+            // JWT에 적힌 userID를 통해 DB에서 usr_seq를 가져옴
+            const user = await db['tb_user'].findOne({
+                where: {usr_id: decodedUserToken.userId}
+            });
+
+            // DB tb_car테이블에 저장
+            const car = await db['tb_car'].create({
+                car_num: req.body['carNum'],
+                car_img: req.file.path,
+                car_rent_insurance_yn: req.body['rentInsurance'],
+                car_reg_dt: Date.now(),
+                car_model: req.body['carModel'],
+                car_segment: req.body['carSegment'],
+                car_fuel: req.body['carFuel'],
+                car_rate: req.body['carRate'],
+                usr_seq: user.usr_seq,
+                car_year: req.body['carYear']
+            });
+            return res.status(200).json({status_code: 0});
+        }
+        else {
+            return res.status(400).json({status_code: 1})
         }
     } catch (error) {
         console.log(error);
-        return res.json({carRegister:'fail'});
+        return res.status(400).json({status_code: 1});
     }
 });
 
@@ -43,10 +73,10 @@ router.post('/register', upload.single('carRegisterImage'), async(req, res) => {
 router.post('/insurance', async(req, res) => {
     console.log(req.body);
     try {
-        return res.json({insurance:'success'});
+        return res.status(200).json({insurance:'success'});
     } catch (error) {
         console.log(error);
-        return res.json({insurance:'fail'});
+        return res.status(400).json({insurance:'fail'});
     }
 });
 
