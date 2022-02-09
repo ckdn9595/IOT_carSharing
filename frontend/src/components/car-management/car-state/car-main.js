228e3fe433eb0e1d257ec8a28d48a4aefd9795a1
park <horusr8@gmail.com>
2022-02-09 11:15:55 +0900

    Feature: [S06P12A104-9] update component

--- frontend/src/components/car-management/car-state/car-main.js
+++ frontend/src/components/car-management/car-state/car-main.js
@@ -20,30 +20,51 @@ import {
   FormControl,
 } from '@mui/material';
 import CarRegister from './car-register';
+import axios from 'axios';
+
+const OpenCarState = (props) => {
+  const {register} =props
+return (
+  <>
+  {register.map(car =>(
+  <CarState key={car.carNum} car={car}/>
+  ))}
+  </>
+)}
+
 const CarMain = () =>{
   const [register, setRegister] = useState([])
   const [visible, setVisible] = useState(false)
 
-
+  // const getCarList = async() =>{
+  //   const response = await axios.get(
+  //     url = `http://localhost:8001/api/users/${carId}/info`
+  //   )
+  // }
   const getData = async() =>{
     try{
       // const response = await axios({
       //   url =`http://localhost:3000/api/car/${carId}/info`,
       //   method:'GET',
       // })
-      const response = await axios.get(
-        url =`http://localhost:3000/api/car/${carId}/info`
-        )
-      console.log(response.data)
-      setRegister(response.data)
+      // const response = await axios.get(
+      //   url =`http://localhost:8001/api/car/${carId}/info`
+      //   )
+      // console.log(response.data)
+      // setRegister(response.data)
+      console.log(register)
     } catch (error){
       console.log('등록된 차량이없스빈다.')
     }
   }
+  
+
+  
 
   useEffect( ()=> {
     getData()
-  },[])
+
+  },[register])
 
 
   const onClickHandle = () =>{
@@ -51,19 +72,60 @@ const CarMain = () =>{
   }
 
 
-
   return(
     <>
-    <Button
-      variant='contained'
-      onClick={onClickHandle}
+    <Box
+      sx={{display: 'inline-flex',
+      m: 1,
+      p: 1,
+      justifyContent:'space-around',
+      border: '1px solid'
+    }}
     >
-      차량 등록
-    </Button>
+      <Typography
+        variant='h6'
+        lineHeight='3'
+      >
+       차량 관리
+      </Typography>
+      <Button
+        variant='contained'
+        onClick={onClickHandle}
+      >
+        차량 등록
+      </Button>
+    </Box>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        p: 1,
+        m: 1,
+        border: '1px solid',
+      }}
+    >    
     {/* map */}
-    {visible  ? <CarRegister setVisible={setVisible}/>:''}
-    {register === ![] ?    <CarState/>:''}
+     <Grid item xs={12} sx={{border: '1px solid'}} >
+    {visible  ? <CarRegister register={register} setRegister={setRegister}  setVisible={setVisible}/>:''}
+      </Grid>
+    </Box>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        p: 1,
+        m: 1,
+        border: '1px solid',
 
+      }}
+      >
+        <Grid item xs={12} sx={{border: '1px solid'}} >
+      { 1 === 1? <OpenCarState register={register} /> : <Typography variant='h5' > 등록된 차량이 없습니다. </Typography> }
+        </Grid>
+
+    </Box>
     </>
   )
 }
--- frontend/src/components/car-management/car-state/car-state.js
+++ frontend/src/components/car-management/car-state/car-state.js
@@ -28,37 +28,37 @@ import {
 // 수정 삭제
 // 버튼은 
 const dump = {
-  car_name:'good car',
-  car_kind:'sonata',
-  car_year:'2021',
-  car_pic: {
+  carName:'good car',
+  carKind:'sonata',
+  carYear:'2021',
+  carPic: {
     pic_out:['img','img2'],
     pic_in:[''],
     },
-  car_intro:'',
+  carIntro:'',
 }
-const CarState = () =>{
+const CarState = (props) =>{
   const [data, setData]= useState({})
   const [time, setTime] = useState(false)
   const [history, setHistory] = useState(false)
   const [insurance, setInsurance] = useState(false)
 
-  const carId = 'prop.users_car'
-  const getData = async() =>{
-    try{
-      // const response = await axios({
-      //   url =`http://localhost:3000/api/car/${carId}/info`,
-      //   method:'GET',
-      // })
-      const response = await axios.get(
-        url =`http://localhost:3000/api/car/${carId}/info`
-        )
-      console.log(response.data)
-      setData(response.data)
-    } catch (error){
-      alert('error!')
-    }
-  }
+  // const carId = prop.users_car
+  // const getData = async() =>{
+  //   try{
+  //     // const response = await axios({
+  //     //   url =`http://localhost:3000/api/car/${carId}/info`,
+  //     //   method:'GET',
+  //     // })
+  //     const response = await axios.get(
+  //       url =`http://localhost:3000/api/car/${carId}/info`
+  //       )
+  //     console.log(response.data)
+  //     setData(response.data)
+  //   } catch (error){
+  //     alert('error!')
+  //   }
+  // }
   const delData = async() =>{
     try{
       // const response = await axios({
@@ -74,28 +74,72 @@ const CarState = () =>{
       alert('error!')
     }
   }
-
+  useEffect(()=>{
+    setData(props.car)
+  },[])
   
   return(
     <>
-      <Grid>
-        <Grid>
-          차종
-          차량번호
-        </Grid>
-        <Grid>
+    <Box container
+      sx={{display:'flex',
+          border:'1px solid',
+          flexDirection:'column',
+          alignItems:'center',
+          }}
+    >
+      <Box 
+        sx={{ display:'flex',
+              m: 1,
+              justifyContent:'space-between',
+              border: '1px solid'
+        }}
+      >
+          <Typography
+            variant='h6'
+            lineHeight={5}
+          > 차량사진 </Typography>
+          <Box sx={{display:'flex',
+                    flexDirection:'column',
+                    border:'1px solid',
+                    alignItems:'center'
+                    }}>
+
+          <Typography>
+            차종입니다  {data.carKind}
+          </Typography>
+          <Typography>
+            차량번호입니다 {data.carYear}
+          </Typography>
+          </Box>
+        <Box>
         <Button 
             variant="contained"
             color="secondary"
             onClick={()=>{delData}}
-
-          >
+            >
             차량삭제
         </Button>
-        </Grid>
-        <Grid>
-        </Grid>
-        <Button 
+        <Typography>
+          임대상태
+        </Typography>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{display:'flex',
+        flexDirection: 'column',
+      
+      }}
+      >
+      <Grid
+        sx={{display: 'flex',     
+            border:"1px solid",
+            alignItems: 'flex-start',
+            p: 1,
+            justifyContent:'center',
+            }}
+      >
+      <Button 
             variant="contained"
             color="primary"
             onClick={()=>{setInsurance(!insurance)}}
@@ -109,6 +153,7 @@ const CarState = () =>{
           >
             임대기간 설정
         </Button>
+        
         <Button 
             variant="contained"
             color="primary"
@@ -116,10 +161,15 @@ const CarState = () =>{
           >
             이용내역 보기
         </Button>
+        </Grid>
+        <Grid item xs={12}>
+
         {insurance? <Insurance/>:''}
         {time? <RentPeriod/>:''}
-    {history? <RentHistoryList/>:''}
-      </Grid>
+        {history? <RentHistoryList/>:''}
+        </Grid>
+    </Box>
+    </Box>
     </>
 
   )
