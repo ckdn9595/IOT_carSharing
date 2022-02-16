3024afe3e16797e8049ee968c66bddfe46638676
전현민 <ssiluk12345@gmail.com>
2022-02-16 15:30:45 +0900

    Fix: Edit GetCarInfo.js
    Try to fix img error and etc

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -134,8 +134,8 @@ router.get('/info', async (req, res) => {
     console.log(req.body);
     try {
         // get token from header
-        const userToken = req.header('access_token') || req.headers['x-access-token'] || req.headers['authorization'] || req.headers['Authorization'];
-        if(userToken.indexOf(/^Bearer\s+/) !== -1){
+        let userToken = req.header('access_token') || req.headers['x-access-token'] || req.headers['authorization'] || req.headers['Authorization'];
+        if(userToken.search(/^Bearer\s+/) !== -1){
             userToken = userToken.replace(/^Bearer\s+/,'');
         }
         
