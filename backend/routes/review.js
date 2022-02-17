5e63e67901528e079cd9d4ab8071796f52393a20
전현민 <ssiluk12345@gmail.com>
2022-02-17 17:08:02 +0900

    Fix: Edit review.js
    Delete POST /api/review/:carID/review

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -40,23 +40,50 @@ router.get('/:carID/review', async(req, res) => {
 });
 
 // 특정 차량 리뷰 작성
-router.post('/:carID/review', async(req, res) => {
-    console.log(req.body);
-    try {
-        const fullToken = req.headers['x-access-token'] || req.headers['authorization'] || req.headers['access_token'];
-        const token = fullToken.replace(/^Bearer\s+/, "");
-        const decodedUserToken = await jwt.verify(token, process.env.JWT_SECRET);
+// router.post('/:carID/review', async(req, res) => {
+//     console.log(req.body);
+//     try {
+//         const fullToken = req.headers['x-access-token'] || req.headers['authorization'] || req.headers['access_token'];
+//         const token = fullToken.replace(/^Bearer\s+/, "");
+//         const decodedUserToken = await jwt.verify(token, process.env.JWT_SECRET);
 
-        const userId = await db['tb_user'].findOne({
-            where: {usr_id: decodedUserToken.userId}
-        });
+//         const userId = await db['tb_user'].findOne({
+//             where: {usr_id: decodedUserToken.userId}
+//         });
 
+//         if (req.body['car_res_seq', 'rev_content']) {
+//             const myReviewExist = await db['tb_car_review'].findOne({
+//                 where: {
+//                     usr_seq: userId.usr_seq,
+//                     car_seq: req.params.carID,
+//                     car_res_seq: req.body['car_res_seq']
+//                 }
+//             });
+
+//             if (myReviewExist) {
+//                 return res.status(400).json({message: 'your review already exists'});
+//             } else {
+//                 const owner = await db['tb_car'].findOne({
+//                     where: {car_seq: req.params.carID}
+//                 });
+
+//                 const reviewPost = await db['tb_car_review'].create({
+//                     car_res_seq: req.body['car_res_seq'],
+//                     owner_seq: owner.usr_seq,
+//                     usr_seq: userId.usr_seq,
+                    
+//                 });
+//             }
+//         } else {
+//             return res.status(400).json({message: 'there must be req.body.car_res_seq AND req.body.rev_content'});
+//         }
         
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({statusCode: 1});
-    }
-});
+        
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(400).json({statusCode: 1});
+//     }
+// });
 
 // JWT 유저의 리뷰 목록
 router.get('/myreview', async (req, res) => {
@@ -80,6 +107,30 @@ router.get('/myreview', async (req, res) => {
     }
 });
 
+// 특정 차량에 대한 JWT 유저의 리뷰 목록
+router.get('/:carID/myreview', async (req, res) => {
+    console.log(req.body);
+    try {
+        const fullToken = req.headers['x-access-token'] || req.headers['authorization'] || req.headers['access_token'];
+        const token = fullToken.replace(/^Bearer\s+/, "");
+        const decodedUserToken = await jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = await db['tb_user'].findOne({
+            where: {usr_id: decodedUserToken.userId}
+        });
+
+        const myReview = await db['tb_car_review'].findAll({
+            where: {
+                usr_seq: userId.usr_seq,
+                car_seq: req.params.carID
+            }
+        });
+        return res.status(200).json({myReview});
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({statusCode: 1});
+    }
+});
 
 
 module.exports = router;
