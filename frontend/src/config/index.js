8205edad13516034fa6099713aaf31d4c6e01e60
박종선 <shw2@naver.com>
2022-02-18 01:19:30 +0000

    Update index.js

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://i6a104.p.ssafy.io/api"; //aws server url
-const API_BASE_URL = "http://localhost:8001/api"; //local server url
+const API_BASE_URL = "https://i6a104.p.ssafy.io/api"; //aws server url
+//const API_BASE_URL = "http://localhost:8001/api"; //local server url
 //const LOCAL_BASE_URL = "http://localhost:8000";
 //const KAKAO_MAP_URL = "kakao.map.com";
 
